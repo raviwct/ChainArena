48ad33c5e2cfe379ca45300dc6c8de56ba17e7ac
Nitin.Lakum <lakumnitin1@gmail.com>
2022-09-07 16:40:23 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const content = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default content;
